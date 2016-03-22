7573a38712377b1b798a6be1b34cca6c3abacbbc
csos95 <csoscss@gmail.com>
2016-03-22 14:45:27 -0500

    Add door stuff
    Start on bolin internal door model, bolin-f1-door-test
    (adding cutouts for doors to the bolin-f1 model)
    and make scripts and animations for a door prefab in unity

---
+++ unity/testing/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputHandler : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rayCastHit;
+
+            if (Physics.Raycast(ray.origin, ray.direction, out rayCastHit, Mathf.Infinity))
+            {
+                Debug.Log("Mouse Clicked!");
+                Door door = rayCastHit.transform.GetComponent<Door>();
+                door.PlayAnimation();
+            }
+        }
+    }
+}
